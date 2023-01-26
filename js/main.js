1f677903b1049f73dabdfbaa51944ecbb3b6600d
Fernando Hiroshi <fernandohiroshidev@gmail.com>
2023-01-26 15:19:05 -0300

    modulo 10 | plugins jquery
    commit 1

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+
+
+    $('#telefone').mask('(00) 00000-0000');
+    $('#cpf').mask('000.000.000.00');
+    $('#cep').mask('00000-000');
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true, 
+            
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+        },
+        submitHandler: function(form) {
+            e,preveventDefault();
+        },
+        
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`${camposIncorretos} campos não preenchidos no formulário de contato, por gentileza preencher todos os campos para enviar.`);
+            }
+        }
+    })
+})
+
+
+
+
